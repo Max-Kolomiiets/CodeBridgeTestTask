8346132636ca4510c513ac19f661cabdbf73d0e7
Max-Kolomiiets <kmaks1364@gmail.com>
2022-01-19 02:36:14 +0200

    created test for PingController

--- CodeBridgeTestTask.Tests/DogsControllerTest.cs
+++ CodeBridgeTestTask.Tests/DogsControllerTest.cs
@@ -3,11 +3,8 @@ using CodeBridgeTestTask.Core.Entities;
 using CodeBridgeTestTask.DAL.Helpers;
 using CodeBridgeTestTask.Infrastructure.Data.Repositories;
 using Moq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace CodeBridgeTestTask.Tests
@@ -20,12 +17,6 @@ namespace CodeBridgeTestTask.Tests
 
         public DogsControllerTest()
         {
-            
-        }
-
-        [Fact]
-        public async void GetDogs_ShouldReturnDogs()
-        {
             IList<Dog> dogs = new List<Dog>()
             {
                 new Dog("Neo", "red & amber", 22, 32),
@@ -35,11 +26,28 @@ namespace CodeBridgeTestTask.Tests
 
             mock.Setup(rep => rep.GetDogsAsync(null, null))
                 .ReturnsAsync(pl);
+            mock.Setup(rep => rep.AddDogAsync(It.IsAny<Dog>()))
+               .Callback((Dog dog) =>
+               {
+                   dog.Id = dogs.Count + 1;
+                   dogs.Add(dog);
+               });
+            mock.SetupAllProperties();
+        }
 
+        [Fact]
+        public async void GetDogs_ShouldReturnDogs()
+        {
             DogsController controller = new DogsController(mock.Object);
-
             var result = await controller.GetAllAsync(null, null);
+            Assert.NotNull(result);
+        }
 
+        [Fact]
+        public async void AddDog_ShouldCreateANewDog_ReturnsCreated_Dog()
+        {
+            DogsController controller = new DogsController(mock.Object);
+            var result = await controller.CreateAsync(new Dog("Lucky", "red", 12, 17));
             Assert.NotNull(result);
         }
     }
---
+++ CodeBridgeTestTask.Tests/PingControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using CodeBridgeTestTask.API.Controllers;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeBridgeTestTask.Tests
+{
+    public class PingControllerTest
+    {
+        private readonly Mock<IConfiguration> mockConfig = new Mock<IConfiguration>();
+
+        public PingControllerTest()
+        {
+            mockConfig.Setup(x => x.GetSection("DogService")["Version"])
+                .Returns("1.0.1");   
+        }
+
+        [Fact]
+        public void Ping_ShouldReturnStringVersion()
+        {
+            PingController controller = new PingController(mockConfig.Object);
+            var result = controller.Ping();
+            Assert.NotNull(result);
+        }
+    }
+}
